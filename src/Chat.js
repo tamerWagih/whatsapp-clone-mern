45207b3a3bb177d6a46c2b1c3e51850ff90fbb23
Tamer Wagih <mrtamerwagih@gmail.com>
2021-05-19 13:56:38 +0300

    initial commit, adding basic components

---
+++ src/Chat.js
@@ -0,0 +1,56 @@
+import { Avatar, IconButton } from '@material-ui/core';
+import { AttachFile, MoreVert, SearchOutlined } from '@material-ui/icons';
+import InsertEmoticonIcon from '@material-ui/icons/InsertEmoticon';
+import MicIcon from '@material-ui/icons/Mic';
+import React from 'react';
+import './Chat.css';
+
+function Chat() {
+  return (
+    <div className="chat">
+      <div className="chat__header">
+        <Avatar />
+        <div className="chat__headerInfo">
+          <h3>Room name</h3>
+          <p>last seen at...</p>
+        </div>
+        <div className="chat__headerRight">
+          <IconButton>
+            <SearchOutlined />
+          </IconButton>
+          <IconButton>
+            <AttachFile />
+          </IconButton>
+          <IconButton>
+            <MoreVert />
+          </IconButton>
+        </div>
+      </div>
+
+      <div className="chat__body">
+        <p className="chat__message">
+          <span className="chat__name">Sonny</span>
+          This is a message
+          <span className="chat__timestamp">{new Date().toUTCString()}</span>
+        </p>
+
+        <p className="chat__message chat__reciever">
+          <span className="chat__name">Sonny</span>
+          This is a message
+          <span className="chat__timestamp">{new Date().toUTCString()}</span>
+        </p>
+      </div>
+
+      <div className="chat__footer">
+        <InsertEmoticonIcon />
+        <form>
+          <input type="text" placeholder="Type a message" />
+          <button type="submit">Send a message</button>
+        </form>
+        <MicIcon />
+      </div>
+    </div>
+  );
+}
+
+export default Chat;
---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: 'AIzaSyCsPJOrSNfHDjHdtL1AgZ6ZR04Vf0hJMXI',
+  authDomain: 'whatsapp-clone-706f6.firebaseapp.com',
+  projectId: 'whatsapp-clone-706f6',
+  storageBucket: 'whatsapp-clone-706f6.appspot.com',
+  messagingSenderId: '154487133706',
+  appId: '1:154487133706:web:9263add9974c3f4c2db1f3',
+};
